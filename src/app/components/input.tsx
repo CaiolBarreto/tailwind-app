12d1c5a3da71436267e1da4674244e9bc596df08
Caio Barreto <caiobarreto_28@hotmail.com>
2023-08-24 08:50:47 -0300

    sidebar completed

---
+++ src/app/components/input.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps } from 'react'
+
+type InputPrefixProps = ComponentProps<'div'>
+
+export function InputPrefix(props: InputPrefixProps) {
+  return <div {...props} />
+}
+
+type InputControl = ComponentProps<'input'>
+
+export function InputControl(props: InputControl) {
+  return (
+    <input
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      {...props}
+    />
+  )
+}
+
+type InputRoot = ComponentProps<'div'>
+
+export function InputRoot(props: InputRoot) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    />
+  )
+}
